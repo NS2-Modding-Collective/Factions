3e0dd81ca7f6b005203b7ac2edcdf0ee3a6ca0dd
AlexHayton <GitMongo email>
2013-04-01 13:25:12 +0100

    Add a damage modifier option, which allows us to specify the difficulty in other mods.

--- lua/ExtraEntitiesMod/DamageTypes_modded.lua
+++ lua/ExtraEntitiesMod/DamageTypes_modded.lua
@@ -5,13 +5,13 @@
 //
 //________________________________
 
-
+kNPCDamageModifier = 0.1
 // if its easy you only take half damage
 local originalGetDamageByType = GetDamageByType
 function GetDamageByType(target, attacker, doer, damage, damageType)
       // only for the client    
     if attacker.isaNpc and not target.isaNpc then
-        damage = damage * 0.1
+        damage = damage * kNPCDamageModifier
     end
     return originalGetDamageByType(target, attacker, doer, damage, damageType)
 end
