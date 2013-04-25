36665a8b4ff75e33ba03e7882a0ba26daa83c134
AlexHayton <alex.hayton@gmail.com>
2013-04-25 19:00:06 +0100

    Typo fix

--- lua/Client_Modded.lua
+++ lua/Client_Modded.lua
@@ -1,4 +1,4 @@
-n// ======= Copyright (c) 2003-2013, Unknown Worlds Entertainment, Inc. All rights reserved. =======
+// ======= Copyright (c) 2003-2013, Unknown Worlds Entertainment, Inc. All rights reserved. =======
 //
 // lua\Client_Modded.lua
 //
