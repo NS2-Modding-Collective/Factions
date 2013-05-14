0ce7dacf2e36a2d466692351565361303e5faac1
AlexHayton <alex.hayton@gmail.com>
2013-05-14 19:20:46 +0100

    Made a knife!

---
+++ output/lua/Factions/Weapons/Factions_Knife.lua
@@ -0,0 +1,59 @@
+//________________________________
+//
+//  Factions
+//	Made by Jibrail, JimWest, Sewlek
+//  Puschen and Winston Smith (MCMLXXXIV)
+//  
+//  Licensed under LGPL v3.0
+//________________________________
+
+// Factions_Knife.lua
+
+Script.Load("lua/Weapons/Marine/Axe.lua")
+
+class 'Knife' (Axe)
+
+Knife.kMapName = "knife"
+
+Knife.kModelName = PrecacheAsset("models/marine/knife/knife.model")
+local kViewModelName = PrecacheAsset("models/marine/knife/knife_view.model")
+local kAnimationGraph = PrecacheAsset("models/marine/knife/knife_view.animation_graph")
+
+LightMachineGun.kIronSightTexture = "ui/Factions/testing_ironsights.png"
+LightMachineGun.kIronSightZoomFOV = 80
+LightMachineGun.kIronSightActivateTime = 0.1
+
+LightMachineGun.kLaserSightWorldModelAttachPoint = "fxnode_riflemuzzle"
+LightMachineGun.kLaserSightViewModelAttachPoint = "fxnode_riflemuzzle"
+
+// 4 degrees in NS1
+local kSpread = Vector(0, 0, 0)
+
+local kAttackSoundName = PrecacheAsset("sound/NS2.fev/marine/structures/sentry_fire_loop")
+local kEndSound = PrecacheAsset("sound/NS2.fev/marine/structures/sentry_spin_down")
+
+local networkVars =
+{
+}
+
+function Knife:GetAnimationGraphName()
+    return kAnimationGraph
+end
+
+function Knife:GetViewModelName()
+    return kViewModelName
+end
+
+function Knife:GetSpread()
+    return kSpread
+end
+
+function Knife:GetWeight()
+    return kLightMachineGunWeight
+end
+
+function Knife:OverrideWeaponName()
+    return "knife"
+end
+
+Shared.LinkClassToMap("Knife", Knife.kMapName, networkVars)
