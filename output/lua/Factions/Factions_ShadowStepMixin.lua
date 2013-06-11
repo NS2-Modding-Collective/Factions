f459fe83f678b34b283325e1416bc706895c4d62
AlexHayton <alex.hayton@gmail.com>
2013-06-11 08:47:48 +0100

    Shadow step skeleton

--- output/lua/Factions/Factions_ShadowStepMixin.lua
+++ output/lua/Factions/Factions_ShadowStepMixin.lua
@@ -14,7 +14,12 @@ Script.Load("lua/Factions/Factions_FactionsClassMixin.lua")
 ShadowStepAbilityMixin = CreateMixin( ShadowStepAbilityMixin )
 ShadowStepAbilityMixin.type = "ShadowStepAbility"
 
-ShadowStepAbilityMixin.stepDistance = kFadeShadowStepDistance
+ShadowStepAbilityMixin.kBaseSpeedScalar = 1.2
+ShadowStepAbilityMixin.kSpeedScalarIncrease = 0.1
+ShadowStepAbilityMixin.kBaseTimeBetweenAttacks = 0.4
+ShadowStepAbilityMixin.kTimeBetweenAttacksDecrease = 0.07
+ShadowStepAbilityMixin.kBaseStaminaDrainRate = 0.5
+ShadowStepAbilityMixin.kStaminaDrainRateDecrease = 0.15
 
 ShadowStepAbilityMixin.expectedMixins =
 {
@@ -31,6 +36,8 @@ ShadowStepAbilityMixin.expectedConstants =
 ShadowStepAbilityMixin.networkVars =
 {
 	shadowStepLevel = "integer (0 to " .. #ShadowStepUpgrade.cost .. ")",
+	shadowStepping = "boolean",
+	shadowStepStartTime = "time",
 }
 
 function ShadowStepAbilityMixin:__initmixin()
@@ -38,6 +45,8 @@ function ShadowStepAbilityMixin:__initmixin()
 	if Server then
 		self.shadowStepLevel = 0
 	end
+	
+	self.shadowStepAttackCooldowns = {}
 
 end
 
@@ -49,7 +58,48 @@ function ShadowStepAbilityMixin:CopyPlayerDataFrom(player)
 
 end
 
+function ShadowStepAbilityMixin:GetTimeBetweenAutoAttacks()
+
+	return ShadowStepAbilityMixin.kBaseTimeBetweenAttacks - ShadowStepAbilityMixin.kTimeBetweenAttacksDecrease * self.shadowStepLevel
+
+end
+
+function ShadowStepAbilityMixin:GetSprintBoostScalar()
+
+	return ShadowStepAbilityMixin.kBaseSpeedScalar + ShadowStepAbilityMixin.kSpeedScalarIncrease * self.shadowStepLevel
+
+end
+
+function ShadowStepAbilityMixin:GetStaminaDrainRate()
+
+	return ShadowStepAbilityMixin.kBaseStaminaDrainRate - ShadowStepAbilityMixin.kStaminaDrainRateDecrease * self.shadowStepLevel
+
+end
+
 function ShadowStepAbilityMixin:OnSecondaryAttack()
 	// Copy Shadow Step code here
+	if self.shadowStepLevel > 0 then
+		self.shadowStepping = true	
+		self.shadowStepStartTime = Shared.GetTime()
+	end
+end
+
+function ShadowStepAbilityMixin:OnSecondaryAttackEnd()
+	self.shadowStepping = false
+	self.shadowStepStartTime = nil
+end
+
+function ShadowStepAbilityMixin:OnUpdateRender(deltaTime)
+	// Update visual effect
+	if self.shadowStepping then
+		// Show blink effect, slightky toned down
+	end
+end
+
+function ShadowStepAbilityMixin:OnUpdate(deltaTime)
+	// Update sprint bar
+
+	// Detect nearby enemies
 	
+	// Hit them and play swing animation!
 end
