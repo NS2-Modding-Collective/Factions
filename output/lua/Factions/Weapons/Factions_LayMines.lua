120bc4023255c63817e347ad543803460f79adbc
Alex Hayton <alex.hayton@gmail.com>
2013-07-13 15:53:18 +0100

    Give the welder back when we run out of mines

--- output/lua/Factions/Weapons/Factions_LayMines.lua
+++ output/lua/Factions/Weapons/Factions_LayMines.lua
@@ -16,4 +16,41 @@ function LayMines:GetHUDSlot()
     return kMinesHUDSlot
 end
 
+function LayMines:OnTag(tagName)
+
+    PROFILE("LayMines:OnTag")
+    
+    ClipWeapon.OnTag(self, tagName)
+    
+    if tagName == "mine" then
+    
+        local player = self:GetParent()
+        if player then
+        
+            self:PerformPrimaryAttack(player)
+            
+            if self.minesLeft == 0 then
+            
+                self:OnHolster(player)
+                player:RemoveWeapon(self)
+                player:SwitchWeapon(1)
+				
+				// Give the player their welder back
+				player:GiveItem(Welder.kMapName)
+                
+                if Server then                
+                    DestroyEntity(self)
+                end
+                
+            end
+            
+        end
+        
+        self.droppingMine = false
+        
+    end
+    
+end
+
+
 Class_Reload("LayMines", networkVars)
